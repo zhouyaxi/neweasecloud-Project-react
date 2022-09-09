21e37e1ac4b8e6cb978cc4bf224693e2f561c110
青菜白米粥 <724634300@qq.com>
2022-09-09 15:53:56 +0800

    配置路径别名

---
+++ readme.md
@@ -0,0 +1,7 @@
+# 记录
+
+## 项目创建
+
+```bash
+    yarn create vite <项目名称> --template react-ts
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// node 核心模块  用于解析路径   需要安装“@types/node” 模块
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve:{
+    alias:{
+      '@':path.resolve(__dirname,'./src')
+    }
+  }
 })
